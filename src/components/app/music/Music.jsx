e8aa3a4868fa77df6f3d6ee4c50e878aa4985f14
sunhengzhe <825980764@qq.com>
2017-05-07 00:06:33 +0800

    添加网易云音乐

--- src/components/app/music/Music.jsx
+++ src/components/app/music/Music.jsx
@@ -2,20 +2,38 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import wrapApp from '../wrapApp';
 import './music.css';
+import manifest from './manifest';
 
 class Music extends Component {
 
     static propTypes = {
         DraggableArea: PropTypes.func,
+        closeApp: PropTypes.func,
+    }
+
+    state = {
+        title: 'We Don\'t Talk Anymore',
+        subTitle: 'Someone',
     }
 
     render() {
-        const { DraggableArea } = this.props
+        const { DraggableArea, closeApp } = this.props;
+        const { title, subTitle } = this.state;
+
         return (
             <DraggableArea>
                 <div className="music">
                     <div className="cover">
-                        <div className="intro"></div>
+                        <div className="intro">
+                            <a
+                                className="close-btn"
+                                onClick={() => {
+                                    closeApp(manifest.appid);
+                                }}
+                            ></a>
+                            <h3 className="title">{ title }</h3>
+                            <p className="sub-title">{ subTitle }</p>
+                        </div>
                         <div className="control">
 
                         </div>
